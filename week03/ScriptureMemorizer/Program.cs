8aaa4b31f3265dc9b68d6e63d67471f1b61e01ab
Gamujob <jgamutambuli@byupathway.edu>
2026-04-06 10:49:44 +0300

    Added the creativity for exceding Requirements

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,11 +1,48 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Reference reference = new Reference("Proverbs", 3, 5, 6);
-        Scripture scripture = new Scripture(reference, "Trust in the lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.");
+
+        List<Scripture> scriptures = new List<Scripture>();
+        string[] lines = File.ReadAllLines("scripture.txt");
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split('|');
+
+            if (parts.Length == 5)
+            {
+                string book = parts[0];
+                int chapter = int.Parse(parts[1]);
+                int startVerse = int.Parse(parts[2]);
+                int endVerse = int.Parse(parts[3]);
+                string text = parts[4];
+
+                Reference reference = new Reference(book, chapter, startVerse, endVerse);
+                scriptures.Add(new Scripture(reference, text));
+            }
+            else
+            {
+                string book = parts[0];
+                int chapter = int.Parse(parts[1]);
+                int verse = int.Parse(parts[2]);
+                string text = parts[3];
+
+                Reference reference = new Reference(book, chapter, verse);
+                scriptures.Add(new Scripture(reference, text));
+            }
+
+        }
+        // PICK THE RANDOM SCRIPTURE
+        Random random = new Random();
+        Scripture scripture = scriptures[random.Next(scriptures.Count)];
+
+        // Reference reference = new Reference("Proverbs", 3, 5, 6);
+        // Scripture scripture = new Scripture(reference, "Trust in the lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.");
 
         while (true)
         {
