b9b2ee4b6fdaf879eb053fe52cd8e469fe3ef320
Gamujob <jgamutambuli@byupathway.edu>
2026-04-06 09:03:00 +0300

    week three scripture assignment

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference reference = new Reference("Proverbs", 3, 5, 6);
+        Scripture scripture = new Scripture(reference, "Trust in the lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.");
+
+        while (true)
+        {
+            Console.Clear();
+
+            Console.WriteLine(scripture.GetDisplayText());
+
+            if (scripture.IsCompletelyHidden())
+            {
+                Console.WriteLine("\nAll words are hidden. Program ending.");
+                break;
+            }
+
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to finish:");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+            // hide 3 words each time
+            scripture.HideRandomWords(3); 
+        }
     }
 }
